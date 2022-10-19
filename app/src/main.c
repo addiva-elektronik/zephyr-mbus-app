5719756d36c417bff7073f9f99765b6093057015
Joachim Wiberg <troglobit@gmail.com>
2022-10-19 16:12:40 +0200

    Convert to nRF Common Application Framework
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -1,8 +1,29 @@
+/* M-Bus master module for Zephyr
+ *
+ * Copyright (C) 2022  Addiva Elektronik AB
+ *
+ * Author: Joachim Wiberg <joachim.wiberg@addiva.se>
+ */
+#define MODULE main
+
+#include <zephyr/logging/log.h>
+#include <caf/events/module_state_event.h>
 #include <mbus/mbus.h>
+
+LOG_MODULE_REGISTER(MODULE);
 
 int main(void)
 {
-    return mbus_init();
+	/* Start CAF and signal that main have completed system setup */
+	if (app_event_manager_init()) {
+		LOG_ERR("Application Event Manager not initialized!");
+                return 1;
+        }
+
+        LOG_DBG("Event manager initialized.");
+        module_set_state(MODULE_STATE_READY);
+
+        return 0;
 }
 
 /**

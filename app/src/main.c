3a431fd2b7cdb1f18ee28d1e958b9175ad18745e
Joachim Wiberg <troglobit@gmail.com>
2022-11-02 17:39:56 +0100

    Add support for Zephyr settings subsystem
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -8,22 +8,25 @@
 
 #include <zephyr/logging/log.h>
 #include <caf/events/module_state_event.h>
-#include <mbus/mbus.h>
+#include <zephyr/settings/settings.h>
 
 LOG_MODULE_REGISTER(MODULE);
 
 int main(void)
 {
-	/* Start CAF and signal that main have completed system setup */
-	if (app_event_manager_init()) {
-		LOG_ERR("Application Event Manager not initialized!");
-                return 1;
-        }
+    /* Start CAF and signal that main have completed system setup */
+    if (app_event_manager_init()) {
+        LOG_ERR("Application Event Manager not initialized!");
+        return 1;
+    }
 
-        LOG_DBG("Event manager initialized.");
-        module_set_state(MODULE_STATE_READY);
+    settings_subsys_init();
+    settings_load();
 
-        return 0;
+    LOG_DBG("Event manager initialized.");
+    module_set_state(MODULE_STATE_READY);
+
+    return 0;
 }
 
 /**
